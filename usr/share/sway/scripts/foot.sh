92dedde5fe345c9602b415b2cddc9d2de4bd598e
isso <74131447+wdsanoq@users.noreply.github.com>
2022-04-18 17:16:08 +0300

    sway 9

--- usr/share/sway/scripts/foot.sh
+++ usr/share/sway/scripts/foot.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # wrapper script for foot
 
-USER_CONFIG_PATH=${HOME}/.config/foot/foot.ini
+USER_CONFIG_PATH="${HOME}/.config/foot/foot.ini"
 
-if [ -f $USER_CONFIG_PATH ]; then
+if [ -f "$USER_CONFIG_PATH" ]; then
     USER_CONFIG=$USER_CONFIG_PATH
 fi
 
-foot -c ${USER_CONFIG:-"/usr/share/sway/templates/foot.ini"} $@ &
+foot -c "${USER_CONFIG:-"/usr/share/sway/templates/foot.ini"}" "$@" &
